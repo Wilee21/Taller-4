1b587debc2a482679f33f7e329eb6cc1245b8297
Juan Sebastian Cuervo Baquero <99370215+Wilee21@users.noreply.github.com>
2026-06-14 17:41:16 -0500

    d

--- src/main/java/edu/unisabana/tyvs/registry/domain/model/Gender.java
+++
@@ -1,5 +0,0 @@
-package edu.unisabana.tyvs.registry.domain.model;
-
-public enum Gender {
-    MALE, FEMALE
-}
--- src/main/java/edu/unisabana/tyvs/registry/domain/model/RegisterResult.java
+++
@@ -1,5 +0,0 @@
-package edu.unisabana.tyvs.registry.domain.model;
-
-public enum RegisterResult {
-    VALID, DUPLICATED, UNDERAGE, DEAD, INVALID, INVALID_AGE
-}
